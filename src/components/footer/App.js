cb44c1b7bc5e0d4ccb76b12dccb7100e3bb7670f
Tahmeed7653 <tahmeedhm@gmail.com>
2023-12-18 20:50:01 +0600

    base default

---
+++ src/components/footer/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const Footer = () => {
+
+  return (
+    <div className='defaultFooter'>
+      &copy; 2023 Your Company Name. All rights reserved.
+      For More Information 
+
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/navbar/App.js
+++ src/components/navbar/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import './App.css'
 
 const MyNavbar = () => {
   return (
+    
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">Navbar</a>
